8b39cf82146c721021a7001a5fabfad931b4ca9c
Erik Verweij <info@erikverweij.com>
2023-02-16 15:02:08 +0100

    created first draft of inventory stuff

---
+++ src/domain/inventory/Inventory.ts
@@ -0,0 +1,26 @@
+import { Entity } from "../ddd/Entity";
+
+export class Inventory extends Entity {
+  private constructor(
+    private litersOfMilk: number,
+    private numberOfSkins: number
+  ) {
+    super();
+  }
+
+  static MAX_LITERS = 100;
+
+  addMilk(liters: number) {
+    const newLitersOfMilk = this.litersOfMilk + liters;
+
+    if (newLitersOfMilk > Inventory.MAX_LITERS) {
+      throw new Error("Max liters of milk reached");
+    }
+
+    this.litersOfMilk = newLitersOfMilk;
+  }
+
+  static create() {
+    return new this(0, 0);
+  }
+}
