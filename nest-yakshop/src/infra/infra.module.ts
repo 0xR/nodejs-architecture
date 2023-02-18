767b0bc31d935b70565ccaca690bc6b2b7773c8a
Ruben Oostinga <roostinga@xebia.com>
2023-02-18 20:47:44 +0100

    Implement EventDispatcher

--- nest-yakshop/src/infra/infra.module.ts
+++ nest-yakshop/src/infra/infra.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EVENT_DISPATCHER, YAK_REPOSITORY } from '../domain/domain.di-tokens';
-import { EventDispatcher } from './EventDispatcher';
+import { MemoryEventDispatcher } from './memory-event-dispatcher.service';
 import { MemoryYakRepository } from './MemoryYakRepository';
 
 @Module({
   imports: [],
   providers: [
-    { provide: EVENT_DISPATCHER, useClass: EventDispatcher },
+    { provide: EVENT_DISPATCHER, useClass: MemoryEventDispatcher },
     { provide: YAK_REPOSITORY, useClass: MemoryYakRepository },
   ],
   exports: [EVENT_DISPATCHER, YAK_REPOSITORY],
--- nest-yakshop/src/infra/memory-event-dispatcher.service.ts
+++ nest-yakshop/src/infra/memory-event-dispatcher.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { EventEmitter } from 'node:events';
 import { DomainEvent } from '../domain/domain-events';
+import { EventDispatcher } from '../domain/EventDispatcher';
 
 @Injectable()
-export class EventDispatcher {
+export class MemoryEventDispatcher implements EventDispatcher {
   private eventEmitter = new EventEmitter();
 
-  dispatch(...domainEvents: DomainEvent[]) {
+  async dispatch(...domainEvents: DomainEvent[]) {
     domainEvents.forEach((domainEvent) => {
       this.eventEmitter.emit(domainEvent.type, domainEvent);
     });
