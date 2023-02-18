a5a061d08146f38ece200e26b01405732beac367
Ruben Oostinga <roostinga@xebia.com>
2023-02-18 20:08:24 +0100

    Add domain module

--- nest-yakshop/src/app.module.ts
+++ nest-yakshop/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { YakModule } from './modules/yak/yak.module';
+import { DomainModule } from './domain/domain.module';
 
 @Module({
-  imports: [YakModule],
+  imports: [YakModule, DomainModule],
   controllers: [AppController],
   providers: [AppService],
 })
